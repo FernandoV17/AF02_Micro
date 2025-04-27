d4bee7a92f11e425df17739484e54dfdac0f5902
Cherfo <82979766+FernandoV17@users.noreply.github.com>
2025-04-26 18:52:34 -0600

    Libreria de Botones Completada
    Se deja completa la libreria de botones junto con un ejemplo en main para verificar funcionamiento.

--- Firmware/AFU2_Micro/AFU2_Micro/Debug/makedep.mk
+++ Firmware/AFU2_Micro/AFU2_Micro/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+src\BUTTON\BUTTON.c
+
 src\DISPLAY_7SEG_MUX\DISPLAY_7SEG_MUX.c
 
 src\main.c
--- Firmware/AFU2_Micro/AFU2_Micro/src/main.c
+++ Firmware/AFU2_Micro/AFU2_Micro/src/main.c
@@ -3,18 +3,61 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "DISPLAY_7SEG_MUX/DISPLAY_7SEG_MUX.h"
+#include "BUTTON/BUTTON.h"
+
+// Variables globales
+volatile uint16_t counter = 0;  // Cambiado de uint8_t a uint16_t para soportar hasta 9999
+volatile uint8_t display_values[4] = {0, 0, 0, 0};
+
+void update_display_values(void) {
+	display_values[0] = counter / 1000;        // Millares
+	display_values[1] = (counter / 100) % 10;  // Centenas
+	display_values[2] = (counter / 10) % 10;   // Decenas
+	display_values[3] = counter % 10;          // Unidades
+}
 
 int main(void) {
-	// Inicializar display y timer
+	// Inicializar display, timer y botones
 	init_display_mux();
 	timer1_init();
+	buttons_init();
 	
-	update_display(1, 2, 3, 4);
+	// Valores iniciales - ahora con el tipo correcto
+	counter = 1234;
+	update_display_values();
+	update_display(display_values[0], display_values[1],
+	display_values[2], display_values[3]);
 	
+	// Habilitar interrupciones globales
 	sei();
 	
 	while(1) {
-		// Loop principal vacío
-		// El refresco se maneja por interrupciones
+		// Actualizar estados de los botones
+		buttons_update();
+		
+		// Botón 1 (PD2) - Incrementar
+		if(button1_pressed()) {
+			if(counter < 9999) counter++;
+			update_display_values();
+		}
+		
+		// Botón 2 (PD3) - Decrementar
+		if(button2_pressed()) {
+			if(counter > 0) counter--;
+			update_display_values();
+		}
+		
+		// Botón 3 (PD4) - Reset
+		if(button3_pressed()) {
+			counter = 0;
+			update_display_values();
+		}
+		
+		// Actualizar display con los nuevos valores
+		update_display(display_values[0], display_values[1],
+		display_values[2], display_values[3]);
+		
+		// Pequeño retardo para evitar rebotes adicionales
+		_delay_ms(10);
 	}
 }
